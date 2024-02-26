3c2466b7c01f3b1f0b249e0328ab156771544057
baobaybong <baorng@gmail.com>
2024-02-26 13:36:00 +0800

    added CNN improved model

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Recognizing digits from user's drawing
+![demo](images/demo.png)
 
 ## Models:
 - mlp_baseline: Basic MLP trained on MNIST dataset with 91.6% test accuracy. However, this model failed miserably when trying to predict my own handwriting. I ensured no data leakage during training, so the problem is probably that the MNIST dataset is too clean and different from real-life handwriting.
 
 - cnn_baseline: Basic CNN trained on MNIST dataset with 96.5% test accuracy. Same problem as the MLP model.
+
+- cnn_improved: Improved CNN architecture with batch normalization, dropout and data augmentation, 99% test accuracy. Performed much better on my own drawings, probably mainly thanks to the data augmentation.
--- utils.py
+++ utils.py
@@ -18,11 +18,52 @@ def format(img, size=(28, 28)):
     return img_tensor
 
 def predict(img, model_selected):
-    # model_selected="mlp_baseline.pth"
     model_path = f"models/{model_selected}"
     print("Using", model_path)
     model = torch.load(model_path)
+    model.eval()
 
     img = format(img)
 
     return int(torch.argmax(model(img.unsqueeze(0))))
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 32, 3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            
+            nn.Conv2d(32, 32, 3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+
+            nn.Conv2d(32, 32, 3, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+
+            nn.MaxPool2d(2, 2),
+            nn.Dropout(0.25)
+        )
+
+        self.linear = nn.Sequential(
+            nn.Flatten(),
+            
+            nn.Linear(1568, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+
+            nn.Linear(128, 32),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+
+            nn.Linear(32, 10)
+        )
+    
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.linear(x)
+        return x
